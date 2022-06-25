a54b1cb9cb99c0aae9e8acd1157c5d7a0ef3ccd8
Joe <jhliang90us@yahoo.com>
2022-06-25 18:35:52 -0400

    Minimum Index Sum of Two Lists (hash table)

---
+++ src/hash_table/MinimumIndexSumOfTwoLists_1.java
@@ -0,0 +1,38 @@
+package hash_table;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MinimumIndexSumOfTwoLists_1 {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        int min_index=Integer.MAX_VALUE;
+        List<String> restaurants = new ArrayList<>();
+        Map<String, Integer> list1_map=new HashMap<>();
+        for(int i=0; i<list1.length; i++) {
+            list1_map.put(list1[i], i);
+        }
+
+        for(int j=0; j<list2.length; j++) {
+            Integer index=list1_map.get(list2[j]);
+            if(index!=null && index+j<min_index) {
+                min_index=index+j;
+            }
+        }
+
+        for(int j=0; j<list2.length; j++) {
+            Integer index=list1_map.get(list2[j]);
+            if(index!=null && index+j==min_index) {
+                restaurants.add(list2[j]);
+            }
+        }
+
+        String[] res=new String[restaurants.size()];
+        for(int i=0; i<restaurants.size(); i++) {
+            res[i]=restaurants.get(i);
+        }
+
+        return res;
+    }
+}
---
+++ src/hash_table/MinimumIndexSumOfTwoLists_2.java
@@ -0,0 +1,32 @@
+package hash_table;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MinimumIndexSumOfTwoLists_2 {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        int min_index=Integer.MAX_VALUE;
+        List<String> restaurants = new ArrayList<>();
+        Map<String, Integer> list1_map=new HashMap<>();
+        for(int i=0; i<list1.length; i++) {
+            list1_map.put(list1[i], i);
+        }
+
+        for(int j=0; j<list2.length; j++) {
+            Integer index=list1_map.get(list2[j]);
+            if(index!=null) {
+                if(index+j<min_index) {
+                    min_index=index+j;
+                    restaurants.clear();
+                    restaurants.add(list2[j]);
+                } else if(index+j==min_index) {
+                    restaurants.add(list2[j]);
+                }
+            }
+        }
+
+        return restaurants.toArray(new String[restaurants.size()]);
+    }
+}
