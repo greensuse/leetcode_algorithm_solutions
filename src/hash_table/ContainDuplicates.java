793717ac9200488c23329abd023442b189cdf27c
Joe <jhliang90us@yahoo.com>
2022-06-26 16:32:24 -0400

    contain duplicates (hash table)

---
+++ src/hash_table/ContainDuplicates.java
@@ -0,0 +1,20 @@
+package hash_table;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainDuplicates {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set=new HashSet<>();
+        for(int i=0; i<nums.length; i++) {
+            int n = nums[i];
+            if(set.contains(n)) return true;
+            set.add(n);
+            if(set.size()>k) {
+                set.remove(nums[i-k]);
+            }
+        }
+
+        return false;
+    }
+}
