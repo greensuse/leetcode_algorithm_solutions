57de276ffe5ec8e83327b7349d4f77e52b1fb0c8
Joe <jhliang90us@yahoo.com>
2022-06-26 17:00:09 -0400

    logger rate limiter (hash table)

---
+++ src/hash_table/LoggerRateLimiter.java
@@ -0,0 +1,27 @@
+package hash_table;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoggerRateLimiter {
+    Map<String, Integer> map=null;
+
+    public LoggerRateLimiter() {
+        map=new HashMap<>();
+    }
+
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if(map.get(message)==null) {
+            map.put(message, timestamp);
+            return true;
+        }
+
+        int old_timestamp = map.get(message);
+        if(timestamp-old_timestamp>=10) {
+            map.put(message, timestamp);
+            return true;
+        }
+
+        return false;
+    }
+}
