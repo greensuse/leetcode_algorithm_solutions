3944ab0e9aa027ea647ae09305c16aba6b2f1955
Joe <jhliang90us@yahoo.com>
2022-06-30 21:25:35 -0400

    Two Sum III - Data structure design (hash table)

---
+++ src/hash_table/TwoSumIII_1.java
@@ -0,0 +1,28 @@
+package hash_table;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class TwoSumIII_1 {
+    private Map<Integer, Integer> count;
+    public TwoSumIII_1() {
+        count=new HashMap<>();
+    }
+
+    public void add(int number) {
+        count.put(number, count.getOrDefault(number, 0)+1);
+    }
+
+    public boolean find(int value) {
+        Set<Integer> keys = count.keySet();
+        for(int n : keys) {
+            if(n*2!=value && count.get(value-n)!=null && count.get(value-n)>=1) {
+                return true;
+            } else if(n*2==value && count.get(value-n)!=null && count.get(value-n)==2) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/hash_table/TwoSumIII_2.java
@@ -0,0 +1,36 @@
+package hash_table;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class TwoSumIII_2 {
+    public static void main(String[] args) {
+        TwoSumIII_2 twoSum = new TwoSumIII_2();
+        twoSum.add(0);
+        twoSum.add(0);
+        boolean res=twoSum.find(0);
+        System.out.println(res);
+    }
+    private Map<Integer, Integer> count;
+    public TwoSumIII_2() {
+        count=new HashMap<>();
+    }
+
+    public void add(int number) {
+        count.put(number, count.getOrDefault(number, 0)+1);
+    }
+
+    public boolean find(int value) {
+        for(Map.Entry<Integer, Integer> n : count.entrySet()) {
+            int key=n.getKey();
+            int complement = value-key;
+            if(complement!=key && count.containsKey(complement)) {
+                return true;
+            } else if(complement==key &&  n.getValue()>1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
