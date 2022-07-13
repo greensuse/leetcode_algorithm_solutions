d06b389811ca0f824c493e6ee0b35cf182945b67
Joe <jhliang90us@yahoo.com>
2022-07-12 22:25:08 -0400

    binary search (binary search)

---
+++ src/binary_search/BinarySearch.java
@@ -0,0 +1,22 @@
+package binary_search;
+
+public class BinarySearch {
+    public int search(int[] nums, int target) {
+        return doSearch(nums, 0, nums.length-1, target);
+    }
+
+    private int doSearch(int[] nums, int start, int end, int target) {
+        if(start>end) return -1;
+        int mid=start+(end-start)/2;
+        int mid_value=nums[mid];
+
+        if(mid_value==target) return mid;
+
+        if(target<mid_value) {
+            return doSearch(nums, start, mid-1, target);
+        } else {
+            return doSearch(nums, mid+1, end, target);
+        }
+
+    }
+}
