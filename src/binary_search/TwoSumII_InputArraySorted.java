d083920e79c4c4ba61d82c58f12378f2d44e3561
Joe <jhliang90us@yahoo.com>
2022-07-16 21:08:23 -0400

    Two Sum II - Input array is sorted (binary search)

---
+++ src/binary_search/TwoSumII_InputArraySorted.java
@@ -0,0 +1,17 @@
+package binary_search;
+
+public class TwoSumII_InputArraySorted {
+    public int[] twoSum(int[] numbers, int target) {
+        int p1=0, p2=numbers.length-1;
+        while(p1<p2) {
+            if(numbers[p1]+numbers[p2]==target) {
+                break;
+            } else if(numbers[p1]+numbers[p2]<target) {
+                p1++;
+            } else {
+                p2--;
+            }
+        }
+        return new int[] {p1+1, p2+1};
+    }
+}
