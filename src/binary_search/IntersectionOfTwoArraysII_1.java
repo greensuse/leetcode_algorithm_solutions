5e023f70b7fa13f8f1dbf7eeeb3943867bb05130
Joe <jhliang90us@yahoo.com>
2022-07-16 21:04:41 -0400

    Intersection of Two Arrays II (binary search)

---
+++ src/binary_search/IntersectionOfTwoArraysII_1.java
@@ -0,0 +1,37 @@
+package binary_search;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class IntersectionOfTwoArraysII_1 {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if(nums1.length>nums2.length) {
+            return intersect(nums2, nums1);
+        }
+
+        Map<Integer, Integer> map=new HashMap<>(nums1.length);
+
+        for(int n : nums1) {
+            map.put(n, map.getOrDefault(n, 0)+1);
+        }
+
+        List<Integer> overlap=new ArrayList<>();
+        for(int n : nums2) {
+            int count=map.getOrDefault(n,0);
+            if(count>0) {
+                overlap.add(n);
+                map.put(n, count-1);
+            }
+        }
+
+        int[] res=new int[overlap.size()];
+        int i=0;
+        for(int n: overlap) {
+            res[i++]=n;
+        }
+        return res;
+
+    }
+}
