d2d48c555c5bae414ef6c8f916aceaaff2f00ec5
Joe <jhliang90us@yahoo.com>
2022-07-27 14:41:42 -0400

    Number of Provinces (Graph)

---
+++ src/graph/NumberOfProvinces.java
@@ -0,0 +1,26 @@
+package graph;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NumberOfProvinces {
+    public int findCircleNum(int[][] isConnected) {
+        int n=isConnected.length;
+        PathCompressionOptimization disjointSet = new PathCompressionOptimization(n);
+        for(int i=0; i<n; i++) {
+            for(int j=i; j<n; j++) {
+                if(isConnected[i][j]==1) {
+                    disjointSet.union(i, j);
+                }
+            }
+        }
+
+        Set<Integer> rootSet=new HashSet<>();
+        for(int i=0; i<n; i++) {
+            rootSet.add(disjointSet.find(i));
+        }
+
+        return rootSet.size();
+    }
+}
+
