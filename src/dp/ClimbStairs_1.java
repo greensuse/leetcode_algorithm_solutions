300d7e628eaf4d029245fe274dad0c44f510175c
Joe <jhliang90us@yahoo.com>
2022-08-21 09:23:32 -0400

    climbing stairs (dynamic programming)

---
+++ src/dp/ClimbStairs_1.java
@@ -0,0 +1,20 @@
+package dp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//top-down implementation
+public class ClimbStairs_1 {
+    private Map<Integer, Integer> memo=new HashMap<Integer, Integer>();
+    private int dp(int i) {
+        if(i<=2) return i;
+        if(!memo.containsKey(i)) {
+            memo.put(i, dp(i-1)+dp(i-2));
+        }
+        return memo.get(i);
+    }
+
+    public int climbStairs(int n) {
+        return dp(n);
+    }
+}
---
+++ src/dp/ClimbStairs_2.java
@@ -0,0 +1,16 @@
+package dp;
+
+//bottom-up implementation
+public class ClimbStairs_2 {
+    public int climbStairs(int n) {
+        if(n==1) return 1;
+
+        int[] steps=new int[n+1];
+        steps[1]=1; // Base cases
+        steps[2]=2; // Base cases
+        for(int i=3; i<=n; i++) {
+            steps[i]=steps[i-1]+steps[i-2];
+        }
+        return steps[n];
+    }
+}
