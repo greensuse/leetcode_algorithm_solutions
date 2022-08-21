38ce66276457bd09b10987a089990210d6f3cc60
Joe <jhliang90us@yahoo.com>
2022-08-21 14:38:06 -0400

    min cost climbing stairs (dynamic programming)

---
+++ src/dp/MinCostClimbingStairs.java
@@ -0,0 +1,23 @@
+package dp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MinCostClimbingStairs {
+    int[] cost;
+    Map<Integer, Integer> memo=new HashMap<>();
+    public int minCostClimbingStairs(int[] cost) {
+        this.cost=cost;
+        return dp(cost.length);
+    }
+    private int dp(int i) {
+        if(i<=1) return 0;
+
+        if(!memo.containsKey(i)) {
+            int downOne=dp(i-1)+cost[i-1];
+            int downTwo=dp(i-2)+cost[i-2];
+            memo.put(i, Math.min(downOne, downTwo));
+        }
+        return memo.get(i);
+    }
+}
