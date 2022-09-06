722ff13e3242463ea3d83f3348472480225649d2
Joe <jhliang90us@yahoo.com>
2022-09-06 14:01:29 -0400

    number of islands - DFS (graph)

---
+++ src/graph/NumberOfIslands_dfs.java
@@ -0,0 +1,27 @@
+package graph;
+
+public class NumberOfIslands_dfs {
+    public int numIslands(char[][] grid) {
+        int res=0;
+        for(int i=0; i<grid.length; i++) {
+            for(int j=0; j<grid[0].length; j++) {
+                res+=dfs(grid, i, j);
+            }
+        }
+
+        return res;
+    }
+
+    private int dfs(char[][] grid, int i, int j) {
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]=='0') {
+            return 0;
+        }
+
+        grid[i][j]='0';
+        dfs(grid, i-1, j);
+        dfs(grid, i+1, j);
+        dfs(grid, i, j-1);
+        dfs(grid, i, j+1);
+        return 1;
+    }
+}
